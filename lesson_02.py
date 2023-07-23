b1f3dcbb35ea75945d3aaebbc469f739699667b9
Katya <ekaterinatkachenko777@gmail.com>
2023-07-23 17:15:26 +0300

    init2

--- lesson_02.py
+++ lesson_02.py
@@ -1,11 +1,25 @@
 # Атрибуты класса
 
-class Person:
-    name = 'Ivan'
-    age = 30
+# class Person:
+#     name = 'Ivan'
+#     age = 30
+#
+# class Book:
+#     name = '1984'
+#     writer = 'Джордж Оруэлл'
+#     pages = 213
 
-class Book:
-    name = '1984'
-    writer = 'Джордж Оруэлл'
-    pages = 213
+# 1.3________________________________
 
+class SuperHero:
+    pass
+
+batman = SuperHero()
+superman = SuperHero()
+
+batman.can_fly = False
+setattr(SuperHero, 'damage', 175)
+
+superman.can_fly = True
+setattr(SuperHero, 'damage', 285)
+setattr(SuperHero, 'alter_ago', 'Клерк Кент')
